e50c6517a5ae3f1e95e6281df276a446ba1b4efa
Zofia <39326905+SofiaQuintana@users.noreply.github.com>
2022-04-19 23:29:49 -0600

    feat: json to csv func (#1)

---
+++ src/pages/home/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { HeaderContainer, Title } from './styles/Header.styles';
+
+export const Header = () => {
+  return (
+    <HeaderContainer>
+        <Title>JSON to CSV</Title>
+    </HeaderContainer>
+  )
+}
+
+export default Header;
---
+++ src/pages/home/components/styles/Header.styles.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const HeaderContainer = styled.div``
+
+export const Title = styled.h1`
+    text-align: center;
+    font-size: 4em;
+`
